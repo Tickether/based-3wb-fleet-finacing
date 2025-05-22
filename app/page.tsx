a8e4d5663b84358a7cd81062291e2c155b80b4ca
Tickether <tickether@gmail.com>
2025-05-22 11:31:55 +0000

    feat: add landing page + components

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,23 @@
-import Image from "next/image";
+import { Wrapper } from "@/components/landing/wrapper";
+
 
 export default function Home() {
   return (
-    <div>
-
+    <div className="relative flex flex-col items-center justify-center h-full">
+        <div
+          className="
+            absolute inset-0
+            bg-[url('/images/dodo.svg')]
+            bg-center bg-cover
+            opacity-23      /* adjust to taste */
+            -z-10
+          "
+        />
+        
+        <div className="w-full bg-yellow-500 text-black py-1 text-center font-bold z-50">
+          <p>⚠️ Test Mode: Do not use real funds. Contact us at <a href="https://t.me/threeWB" className="text-blue-500">https://t.me/threeWB</a> for test tokens.</p>
+        </div>
+        <Wrapper/>
     </div>
   );
 }
