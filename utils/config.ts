556f052bbfea9fae7b4ba33e6f322fe03c792ed8
Tickether <tickether@gmail.com>
2025-06-01 16:50:39 +0000

    feat: remove rainbow + redo config

--- utils/config.ts
+++ utils/config.ts
@@ -1,14 +1,13 @@
+import { injected } from "wagmi/connectors";
+import { cookieStorage, createConfig, createStorage, http } from "wagmi";
+import { base, optimism } from "wagmi/chains";
 
-import { getDefaultConfig } from "@rainbow-me/rainbowkit";
-import { http } from "viem";
-import { base } from "wagmi/chains";
-
-export const config = getDefaultConfig({
-    appName: "3 Wheeler Bike Club",
-    projectId: process.env.NEXT_PUBLIC_WC_PROJECT_ID,
-    chains: [base],
+export const config = createConfig({
+    connectors: [injected()],
+    chains: [base, optimism],
     ssr: true,
     transports: {
-        [base.id]: http(`https://base-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_API_KEY}`),
+      [base.id]: http(),
+      [optimism.id]: http(),
     },
 });
