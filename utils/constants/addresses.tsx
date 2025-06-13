ad343575c09b4d5564d5963feba477e083364eca
Tickether <tickether@gmail.com>
2025-06-13 09:37:46 +0000

    feat: update contracts

--- utils/constants/addresses.tsx
+++ utils/constants/addresses.tsx
@@ -1,4 +1,4 @@
 export const USDC: `0x${string}` = "0x48065fbBE25f71C9282ddf5e1cD6D6A887483D5e";
-export const fleetOrderBook: `0x${string}` = "0xEd3B54036B6dB4F05E96c775f88d548C45ee81Fb";
-export const fleetOrderToken: `0x${string}` = "0xEC9AEB409E09aa6025CA18041777EB3b1A2ffeA6";
+export const fleetOrderBook: `0x${string}` = "0x1e2F50675bee73bAd8f4D2C81cB977222Fbf19f8";
+export const fleetOrderToken: `0x${string}` = "0xB2378a500f70441d7b388b45eBd58B831C26d2D1";
 export const divvi: `0x${string}` = "0xEdb51A8C390fC84B1c2a40e0AE9C9882Fa7b7277";
