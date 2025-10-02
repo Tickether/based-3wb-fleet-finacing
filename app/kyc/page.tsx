b8b973b37c79faddf46acdacfa0ca6ae250c4cbb
Tickether <tickether@gmail.com>
2025-10-02 03:39:53 +0000

    feat: Add initial KYC page component
    Creates the KYC page at app/kyc/page.tsx with a basic layout. The Wrapper component is commented out for future use.

---
+++ app/kyc/page.tsx
@@ -0,0 +1,10 @@
+//import { Wrapper } from "@/components/kyc/wrapper";
+
+export default function Fleet() {
+
+  return (
+    <div className="flex flex-col w-full h-screen">
+      {/*<Wrapper/>*/}
+    </div>
+  );
+}
