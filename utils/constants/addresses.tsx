cd4a519dc53dd211f06bc481f87838b753a322ab
Tickether <tickether@gmail.com>
2025-10-03 06:14:03 +0000

    feat: Update fleet contract addresses
    Changed the addresses for fleetOrderBook and fleetOrderToken to new values. This ensures the application interacts with the correct deployed contracts.

--- utils/constants/addresses.tsx
+++ utils/constants/addresses.tsx
@@ -1,4 +1,4 @@
 export const USDC: `0x${string}` = "0x48065fbBE25f71C9282ddf5e1cD6D6A887483D5e";
-export const fleetOrderBook: `0x${string}` = "0x5847FC36F88707Bf71699Ff735fae6595493779f";
-export const fleetOrderToken: `0x${string}` = "0xF91905A8348d47856207b892b376F1C15FFCCdb9";
+export const fleetOrderBook: `0x${string}` = "0xd96abF68a6D3740D47e826f9b5a089Ab8c3d357C";
+export const fleetOrderToken: `0x${string}` = "0x1ad9495aE2ee49ECf610B08C032af1eF25999878";
 export const divvi: `0x${string}` = "0xEdb51A8C390fC84B1c2a40e0AE9C9882Fa7b7277";
